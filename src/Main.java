213bb9e904c972700a0a125bbbebae509b106260
AymericJak <aymericjakpro@gmail.com>
2024-12-11 11:51:22 +0100

    Manage login

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
 
@@ -13,9 +17,33 @@ public class Main {
             while (true) {
                 // Accepter une connexion client
                 Socket clientSocket = serverSocket.accept();
+
+                // service ready
+                OutputStream outputStream = clientSocket.getOutputStream();
+                outputStream.write("220 Service ready\r\n".getBytes());
                 System.out.println("Nouveau client connecté sur le port : " + clientSocket.getPort());
 
+                // LOGIN
+                InputStream inputStream = clientSocket.getInputStream();
+                Scanner scanner = new Scanner(inputStream);
+                String str = scanner.nextLine();
+
+                if (Objects.equals(str, "USER miage")) {
+                    System.out.println("name : " + str);
+                    outputStream.write("331 username ok\r\n".getBytes());
+                } else {
+                    outputStream.write("530 invalid username\r\n".getBytes());
+                }
+
+
+                // PASSWORD
+
+
+                // Fermeture des connexions
+                //clientSocket.close();
+                //serverSocket.close();
             }
+
         } catch (IOException e) {
             System.err.println("Erreur du serveur : " + e.getMessage());
         }

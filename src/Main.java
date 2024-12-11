6dcf3ad3bc14ac90fea810b67550d212047d999c
AymericJak <aymericjakpro@gmail.com>
2024-12-11 11:05:17 +0100

    Implement client connection

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
+
+    private static final int PORT = 2121;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Serveur FTP, écoute sur le port " + PORT);
+
+            while (true) {
+                // Accepter une connexion client
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Nouveau client connecté sur le port : " + clientSocket.getPort());
+
+            }
+        } catch (IOException e) {
+            System.err.println("Erreur du serveur : " + e.getMessage());
+        }
     }
 }
